a15f475f414ffbfcae537f21eaaff774816be6b1
Najeeb Arif <NajeebArif786@outlook.com>
2021-04-03 03:15:07 +0530

    Removing Security

--- src/main/kotlin/narif/thriftshop/thriftapi/config/UserDetailsConfig.kt
+++ src/main/kotlin/narif/thriftshop/thriftapi/config/UserDetailsConfig.kt
@@ -2,15 +2,15 @@ package narif.thriftshop.thriftapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
+//import org.springframework.security.core.userdetails.User
+//import org.springframework.security.core.userdetails.UserDetailsService
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder
+//import org.springframework.security.crypto.password.PasswordEncoder
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
-@Configuration
+//@Configuration
 class UserDetailsConfig {
-
+/*
     @Bean
     fun userDetailsService(): UserDetailsService {
         val inMemoryUserDetailsManager = InMemoryUserDetailsManager()
@@ -22,5 +22,5 @@ class UserDetailsConfig {
     @Bean
     fun noOpPassword(): PasswordEncoder {
         return NoOpPasswordEncoder.getInstance();
-    }
+    }*/
 }
